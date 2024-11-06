1b6f2bf7c135c55cc56e16ba1787d5533fc7a9b5
ines-di <ines.gomes.dias@tecnico.ulisboa.pt>
2024-11-06 22:05:52 +0000

    physical error in the dispersive error

--- error_calculation.m
+++ error_calculation.m
@@ -3,12 +3,13 @@
 % -------------------------------------------------------------------------
 
 
-function [] = error_calculation(x_max,x_min,n_cells,C,a,method)
+function [diffusive_error,dispersive_error] = error_calculation(x_max,x_min,n_cells,C,a,method)
 
 dx = (x_max-x_min)/(n_cells-1);
 dt = C*dx/a;
 
 k_values = linspace(0, pi/dx, n_cells);
+% k_values = linspace(0, pi);
 
 diffusive_error = zeros(size(k_values));
 dispersive_error = zeros(size(k_values));
@@ -16,11 +17,11 @@ dispersive_error = zeros(size(k_values));
 for idx = 1:length(k_values)
     k = k_values(idx); 
 
-    G_exact = exp(-1i * a * k * dt);
+    % G_exact = exp(-1i*a*k*dt);
     
    switch method
             case 'explicit'  % Explicit Euler with upwind scheme
-                G_numerical = 1 - C + C * (cos(k*dx) - 1i * sin(k*dx));
+                G_numerical = 1 - C + C * (cos(k*dx) - 1i*sin(k*dx));
     
             case 'implicit' % Implicit Euler with upwind scheme
                 G_numerical = 1 / (1 + C * (1 - cos(k*dx) - 1i * sin(k*dx)));
@@ -34,8 +35,8 @@ for idx = 1:length(k_values)
     diffusive_error(idx) = abs(G_numerical);
     
     % Calculate dispersive error as phase difference between G_numerical and G_exact
-    dispersive_error(idx) = abs(angle(G_numerical) - angle(G_exact));
-    % dispersive_error(idx) = angle(G_exact)/angle(G_numerical);
+    % dispersive_error(idx) = abs(angle(G_numerical)-angle(G_exact));
+    dispersive_error(idx) = angle(G_numerical)/(-C*k*dx);
 end
 
 figure;
