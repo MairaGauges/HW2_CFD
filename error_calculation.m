6a72f7915c95a47d3d73efa2bc0312a367aac844
ines-di <ines.gomes.dias@tecnico.ulisboa.pt>
2024-11-06 19:07:44 +0000

    discretization, initial condition and error

---
+++ error_calculation.m
@@ -0,0 +1,54 @@
+% -------------------------------------------------------------------------
+% Dispersive and dissipative error calculation
+% -------------------------------------------------------------------------
+
+
+function [] = error_calculation(x_max,x_min,n_cells,C,a,method)
+
+dx = (x_max-x_min)/(n_cells-1);
+dt = C*dx/a;
+
+k_values = linspace(0, pi/dx, n_cells);
+
+diffusive_error = zeros(size(k_values));
+dispersive_error = zeros(size(k_values));
+
+for idx = 1:length(k_values)
+    k = k_values(idx); 
+
+    G_exact = exp(-1i * a * k * dt);
+    
+   switch method
+            case 'explicit'  % Explicit Euler with upwind scheme
+                G_numerical = 1 - C + C * (cos(k*dx) - 1i * sin(k*dx));
+    
+            case 'implicit' % Implicit Euler with upwind scheme
+                G_numerical = 1 / (1 + C * (1 - cos(k*dx) - 1i * sin(k*dx)));
+    
+            otherwise
+                error_calculation('ups');
+    end
+    
+    % Calculate diffusive error as |G_numerical| - |G_exact|, ideally |G_exact| = 1
+    % diffusive_error(idx) = abs(abs(G_numerical) - 1);
+    diffusive_error(idx) = abs(G_numerical);
+    
+    % Calculate dispersive error as phase difference between G_numerical and G_exact
+    dispersive_error(idx) = abs(angle(G_numerical) - angle(G_exact));
+    % dispersive_error(idx) = angle(G_exact)/angle(G_numerical);
+end
+
+figure;
+subplot(2,1,1);
+plot(k_values, diffusive_error);
+xlabel('Wavenumber');
+ylabel('Diffusive Error');
+title('Diffusive Error');
+
+subplot(2,1,2);
+plot(k_values, dispersive_error);
+xlabel('Wavenumber');
+ylabel('Dispersive Error');
+title('Dispersive Error');
+
+end
--- upwind_expeuler.m
+++ upwind_expeuler.m
@@ -10,55 +10,18 @@ n_cells = 200;
 x = linspace(x_min, x_max, n_cells);
 
 dx = (x_max-x_min)/(n_cells-1); 
-C = 0.995; % Courant
+C = 1; % Courant
 dt = C*dx/a;
 
 % Initial condition
 
-a_elipse = 0.5;
-z = -0.7;
-delta = 0.005;
-alpha = 10;
-beta = log(2)/(36*delta^2);
-
-G = @(x, beta, z) exp(-beta * (x - z).^2);
-
-F = @(x, alpha, a_elipse) sqrt(max(1 - alpha^2 * (x - a_elipse).^2, 0));
-
-phi_0 = zeros(1, n_cells);
-
-for i = 1:n_cells
-    if x(i) >= -0.8 && x(i) <= -0.6
-        phi_0(i) = (1/6) * (G(x(i), beta, z - delta) + G(x(i), beta, z + delta) + 4 * G(x(i), beta, z));
-
-    elseif x(i) >= -0.4 && x(i) <= -0.2
-        phi_0(i) = 1;
-
-    elseif x(i) >= 0 && x(i) <= 0.2
-        phi_0(i) = 1 - abs(10 * (x(i) - 0.1));
-
-    elseif x(i) >= 0.4 && x(i) <= 0.6
-        phi_0(i) = (1/6) * (F(x(i), alpha, a_elipse - delta) + F(x(i), alpha, a_elipse + delta) + 4 * F(x(i), alpha, a_elipse));
-   
-    else
-        phi_0(i) = 0;
-    end
-end
-
-figure;
-plot(x, phi_0, 'k-');
-xlabel('x');
-ylabel('\phi');
-title('Initial Condition');
-
-
-phi = phi_0;   
+phi = initialcondition(n_cells,x_min,x_max); 
 phi_new = phi;
 
 figure;
-plot(x, phi_0,'DisplayName','Initial Condition'); 
+plot(x, phi,'DisplayName','Initial Condition'); 
 hold on;
-eachplot = plot(x, phi,'DisplayName','Solution');
+% eachplot = plot(x, phi,'DisplayName','Solution');
 legend show;
 xlabel('x'); 
 ylabel('\phi');
@@ -68,9 +31,12 @@ for i = 0:dt:2
     phi_new = phi - a*dt/dx * (phi-[phi(end),phi(1:end-1)]);
     phi = phi_new;
 
-    if mod(i,2) < dt
-        set(eachplot, 'YData', phi,'DisplayName', ['dt = ', num2str(i)]);
-        drawnow;
-    end
+
+    % if mod(i,2) < dt
+    %     set(eachplot, 'YData', phi,'DisplayName', ['dt = ', num2str(i)]);
+    %     drawnow;
+    % end
 
 end
+
+plot(x, phi,'DisplayName','Solution');
